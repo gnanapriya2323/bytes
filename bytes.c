f871fa587434a206c3a18f24e410c612e3144318
gnanapriya2323 <priya71054376@gmail.com>
2025-07-24 16:01:29 +0530

    bytes

---
+++ bytes.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/*This programme defines the size of operands*/
+void main()
+{
+	printf("integer:%d\n",sizeof (int));
+	printf("charecter:%d\n",sizeof (char));
+	printf("float:%d\n",sizeof (float));
+	printf("double:%d\n",sizeof (double));
+}
+	
